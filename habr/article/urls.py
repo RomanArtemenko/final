e2e24930ae66d393e06f0220ddcf2d96d3cb80bf
Roman Artemenko <RGArtemenko@gmail.com>
2018-08-11 01:34:09 +0300

    added swagger

--- habr/article/urls.py
+++ habr/article/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path, include
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
 
 from . import views
 from .views import ArticleModelViewSet
+
+schema_view = get_swagger_view(title='Pastebin API')
+
+API_TITLE = 'Article API'
+API_DESCRIPTION = 'A Web API for viewing.'
+
+schema_view = get_swagger_view(title=API_TITLE)
 
 router = DefaultRouter()
 router.register(r'articles', views.ArticleModelViewSet)
@@ -11,4 +20,6 @@ router.register(r'articles', views.ArticleModelViewSet)
 urlpatterns = [
     path('', views.index , name='index'),
     url(r'api/v1.0/', include(router.urls)),
+    path('api/v1.0/map/', schema_view, name='map-api'),
+    path('api/v1.0/docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
 ]
--- habr/habr/settings.py
+++ habr/habr/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'article',
     'rest_framework',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
